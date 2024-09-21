68c59cb170785288d253b1064dffcfdd3ed137a9
fares youssef <124695964+Fares-Youssef@users.noreply.github.com>
2024-09-21 14:25:25 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {IndexComponent} from "./pages/index/index.component";
+import {SignupComponent} from "./auth/signup/signup.component";
+import {LoginComponent} from "./auth/login/login.component";
+import { ProfileComponent } from './user/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'login',component:LoginComponent},
+  {path:'profile',component:ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ProfileComponent } from './user/profile/profile.component';
+import { SidebarComponent } from './user/sidebar/sidebar.component';
+import { AuthInterceptor  } from './interceptor/auth.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent,
+    SignupComponent,
+    LoginComponent,
+    ProfileComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor , multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
